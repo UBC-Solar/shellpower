2f134514a0035788bfcf0954c987be4b15596d4e
Jonah Lee <jonahjacqueslee@gmail.com>
2026-02-18 04:11:24 -0800

    Refactor optimization to extract utility functions

--- shellpower/optimizer/config.py
+++ shellpower/optimizer/config.py
@@ -5,13 +5,13 @@ This defines the environment in which the arrays will be simulated
 """
 import datetime
 
-ncm_motorsports_park_config = {
-    "Latitude": -23.7,
-    "Longitude": 133.8,
-    "HeadingRadians": 3.141592653589793,
-    "Utc": datetime.datetime(2019, 10, 16, 13, 0, 0,
-                             tzinfo=datetime.timezone.utc),
-    "TimezoneOffsetHours": 9.5,
+brainerd_afternoon = {
+    "Latitude": 46.4136132,
+    "Longitude": -94.2774524,
+    "HeadingRadians": 0,
+    "Utc": datetime.datetime(2026, 7, 9, 15+6, 0, 0,
+                             tzinfo=datetime.timezone.utc), # CDT is UTC-6
+    "TimezoneOffsetHours": 0, # unused?
     "TemperatureC": 25.0,
     "DirectIrradianceWm2": 1050.0,
     "DiffuseIrradianceWm2": 70.0
