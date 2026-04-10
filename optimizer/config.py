5cad7ca45a277a4b004ef24c38b38467bcbfd9b2
Jonah Lee <jonahjacqueslee@gmail.com>
2026-04-09 19:53:21 -0700

    Add tool to make brainerd raceway sim configs more easily

--- optimizer/config.py
+++ optimizer/config.py
@@ -4,15 +4,59 @@ Configuration for array optimization conditions
 This defines the environment in which the arrays will be simulated
 """
 
-import datetime
+from datetime import datetime, timedelta, timezone
 from math import pi
+from pprint import pprint
+
+def create_test_case_brainerd(
+        year: int,
+        month: int,
+        day: int,
+        hour: int,
+        heading_deg: float,
+        timezone_offset_hours: float = 0.0,
+        temperature_c: float = 25.0,
+        direct_irradiance_wm2: float = 1050.0,
+        diffuse_irradiance_wm2: float = 70.0,
+    ) -> dict:
+    """
+    Create a test case for the python shelllpower simulator at Brainerd International Raceway
+
+    :param datetime time_utc:
+    :param float heading_deg:
+    :param float timezone_offset_hours:
+    :param float temperature_c:
+    :param float direct_irradiance_wm2:
+    :param float diffuse_irradiance_wm2:
+    """
+
+    hour_offset: int = 13
+    sim_datetime = datetime(year, month, day, (hour + hour_offset) % 24, 0, 0, tzinfo=timezone.utc)
+    # NO IDEA why the time needs this offset, but by experimentation
+    # we find at 2025-07-09 that the peak irradiance is at hour=2 (am), but since the
+    # actual peak irradiance was 1:22PM we say that this was probably actually 1:22PM~1PM.
+    # So to get 12AM brainerd time we need to input hour=13 to shellpower.
+
+    output: dict = {
+        "Latitude": 46.4136132,
+        "Longitude": -94.2774524,
+        "HeadingRadians": pi / 180 * heading_deg,
+        "Utc": sim_datetime,
+        "TimezoneOffsetHours": timezone_offset_hours,  # unused?
+        "TemperatureC": temperature_c,
+        "DirectIrradianceWm2": direct_irradiance_wm2,
+        "DiffuseIrradianceWm2": diffuse_irradiance_wm2,
+    }
+
+    return output
+
 
 brainerd_10am_north = {
     "Latitude": 46.4136132,
     "Longitude": -94.2774524,
     "HeadingRadians": 0,
-    "Utc": datetime.datetime(
-        2026, 7, 9, 23, 0, 0, tzinfo=datetime.timezone.utc
+    "Utc": datetime(
+        2026, 7, 9, 23, 0, 0, tzinfo=timezone.utc
     ),  # CDT is UTC-6
     "TimezoneOffsetHours": 0,  # unused?
     "TemperatureC": 25.0,
@@ -24,8 +68,8 @@ brainerd_1pm_west = {
     "Latitude": 46.4136132,
     "Longitude": -94.2774524,
     "HeadingRadians": pi / 2,
-    "Utc": datetime.datetime(
-        2026, 7, 9, 2, 0, 0, tzinfo=datetime.timezone.utc
+    "Utc": datetime(
+        2026, 7, 9, 2, 0, 0, tzinfo=timezone.utc
     ),  # CDT is UTC-6
     "TimezoneOffsetHours": 0,  # unused?
     "TemperatureC": 25.0,
@@ -37,8 +81,8 @@ brainerd_3pm_east = {
     "Latitude": 46.4136132,
     "Longitude": -94.2774524,
     "HeadingRadians": -pi / 2,
-    "Utc": datetime.datetime(
-        2026, 7, 9, 4, 0, 0, tzinfo=datetime.timezone.utc
+    "Utc": datetime(
+        2026, 7, 9, 4, 0, 0, tzinfo=timezone.utc
     ),  # CDT is UTC-6
     "TimezoneOffsetHours": 0,  # unused?
     "TemperatureC": 25.0,
@@ -50,8 +94,8 @@ brainerd_5pm_south = {
     "Latitude": 46.4136132,
     "Longitude": -94.2774524,
     "HeadingRadians": pi,
-    "Utc": datetime.datetime(
-        2026, 7, 9, 6, 0, 0, tzinfo=datetime.timezone.utc
+    "Utc": datetime(
+        2026, 7, 9, 6, 0, 0, tzinfo=timezone.utc
     ),  # CDT is UTC-6
     "TimezoneOffsetHours": 0,  # unused?
     "TemperatureC": 25.0,
@@ -62,12 +106,10 @@ brainerd_5pm_south = {
 test_cases = [brainerd_10am_north, brainerd_1pm_west, brainerd_3pm_east, brainerd_5pm_south]
 test_case_names = ["brainerd_10am_north", "brainerd_1pm_west", "brainerd_3pm_east", "brainerd_5pm_south"]
 
-if __name__ == "__main__":
-
+def test_time_shift() -> None:
     from pathlib import Path
     from shellpower.simulator.Simulation import Simulation
     from shellpower import ArraySimulator, ArraySimulatorInput, ArraySimulatorOutput
-    import datetime
 
     PROJECT_ROOT = Path(__file__).parent.parent.parent
     BASE_TEXTURE_PATH = PROJECT_ROOT / "arrays" / "v4" / "cascadia_v1_y160x90.png"
@@ -86,8 +128,7 @@ if __name__ == "__main__":
             "Latitude": 46.4136132,
             "Longitude": -94.2774524,
             "HeadingRadians": 0,
-            "Utc": datetime.datetime(2026, 7, 9, hour, 0, 0,
-                                     tzinfo=datetime.timezone.utc),
+            "Utc": datetime(2026, 7, 9, hour, 0, 0, tzinfo=timezone.utc),
             "TimezoneOffsetHours": 0,  # unused?
             "TemperatureC": 25.0,
             "DirectIrradianceWm2": 1050.0,
@@ -104,12 +145,12 @@ if __name__ == "__main__":
 
         output: ArraySimulatorOutput = simulator.simulate(simulator_input)
 
-        print(f"hour: {hour}, power: {output.WattsOutputByCell}")
+        pprint(f"hour: {hour}, power: {output.WattsOutputByCell}")
 
         """
         hour: 0, power: 1352.494454 <-- 11AM
         hour: 1, power: 1453.749220 <-- 12PM
-        hour: 2, power: 1436.234421 <-- NOON is 1:22 on this location/date (so this is about 1pm)
+        hour: 2, power: 1436.234421 <-- SOLAR NOON is 1:22 on this location/date (so this is about 1pm)
         hour: 3, power: 1303.853989 <-- 2pm
         hour: 4, power: 1078.283607 <-- 3pm
         hour: 5, power: 802.0301091 <-- 4pm
@@ -117,14 +158,14 @@ if __name__ == "__main__":
         hour: 7, power: 304.6178147 <-- 6
         hour: 8, power: 163.5031240 <-- 7
         hour: 9, power: 113.0401403 <-- 8
-        hour: 10, power: 102.574004 <-- 9
-        hour: 11, power: 101.933301 <-- 
-        hour: 12, power: 101.558823 <-- 
-        hour: 13, power: 101.441727 <-- 
-        hour: 14, power: 101.438125 <-- 
-        hour: 15, power: 101.438128 <-- 
-        hour: 16, power: 103.364528 <-- 
-        hour: 17, power: 108.114985 <-- 
+        hour: 10, power: 102.574004 <-- 9pm
+        hour: 11, power: 101.933301 <-- 10pm
+        hour: 12, power: 101.558823 <-- 11pm
+        hour: 13, power: 101.441727 <-- 12am / 0am
+        hour: 14, power: 101.438125 <-- 1
+        hour: 15, power: 101.438128 <-- 2
+        hour: 16, power: 103.364528 <-- 3
+        hour: 17, power: 108.114985 <-- 4
         hour: 18, power: 116.955450 <-- 5
         hour: 19, power: 176.385070 <-- 6
         hour: 20, power: 328.707211 <-- 7
@@ -132,3 +173,31 @@ if __name__ == "__main__":
         hour: 22, power: 855.582571 <-- 9
         hour: 23, power: 1135.14076 <-- 10am
         """
+
+if __name__ == "__main__":
+
+    # test_time_shift()
+
+    # 10AM north
+    pprint(create_test_case_brainerd(
+        year=2026, month=7, day=9, hour=10,
+        heading_deg=0
+    ))
+
+    # 1pm west
+    pprint(create_test_case_brainerd(
+        year=2026, month=7, day=9, hour=13,
+        heading_deg=90
+    ))
+
+    # 3pm east
+    pprint(create_test_case_brainerd(
+        year=2026, month=7, day=9, hour=15,
+        heading_deg=-90
+    ))
+
+    # 5pm south
+    pprint(create_test_case_brainerd(
+        year=2026, month=7, day=9, hour=17,
+        heading_deg=180
+    ))
